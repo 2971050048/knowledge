4ee8aed143a7b313a14064d2b23a0530524a4fae
perhaps <perhapszql@gmail.com>
2017-05-18 06:58:44 +0800

    init

---
+++ js/javascript/js-event.js
@@ -0,0 +1,13 @@
+// var btn = document.getElementById('btn');
+// btn.onclick = function() {
+//   alert('btn clicked');
+//   event.stopPropagation();
+// }
+// document.body.onclick = function() {
+//   alert('body clicked');
+// }
+
+true && console.log('1');
+false && console.log('2');
+true || console.log('3');
+false || console.log('4');
---
+++ 工程化/gulp.js
@@ -0,0 +1,4 @@
+// https://github.com/lisposter/gulp-docs-zh-cn
+// http://www.gulpjs.com.cn/docs/
+// http://www.bluesdream.com/blog/gulp-frontend-automation-plugins-instructions.html
+// https://developers.google.com/web/fundamentals/getting-started/web-starter-kit/
