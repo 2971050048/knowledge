53408b4636e60cd3ca1aea4e575dbf0b0ed2944f
perhaps <1728947130@qq.com>
2017-05-18 21:48:04 +0800

    learn how to use chrome dev-tools and debug in vscode

--- 工程化/gulp.js
+++ 工程化/gulp.js
@@ -2,3 +2,4 @@
 // http://www.gulpjs.com.cn/docs/
 // http://www.bluesdream.com/blog/gulp-frontend-automation-plugins-instructions.html
 // https://developers.google.com/web/fundamentals/getting-started/web-starter-kit/
+console.log('hello');
